b6d095e9653972c5212db3d1bfb4c0a8876d3b51
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-11-14 16:47:19 +0530

    changes

---
+++ src/CoalShortagePortal/CoalShortagePortal.WebApp/Controllers/UserManageController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoalShortagePortal.WebApp.Controllers
+{
+    public class UserManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
